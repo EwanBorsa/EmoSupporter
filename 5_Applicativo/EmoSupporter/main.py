ca83871e5d763fec0396dfbac005820c99fe8c3d
Ewan <ewan.borsa@samtrevano.ch>
2022-10-07 14:45:23 +0200

    14:45

--- 5_Applicativo/EmoSupporter/main.py
+++ 5_Applicativo/EmoSupporter/main.py
@@ -1,18 +1,115 @@
 import pystray
-import PIL.Image
+from PIL import Image
+# from PIL.Image import core as _imaging
+import PySimpleGUI as sg
+import cv2
+import numpy as np
 
-image = PIL.Image.open("emo-sup.png")
+iconDir = './assets/emo-sup.png'
+
+
+def conf():
+    sg.theme("LightGreen")
+
+    # Define the window layout
+    layout = [
+        [sg.Text("Settings", size=(60, 1), justification="center")],
+        [sg.Radio("None", "Radio", True, size=(10, 1))],
+        [
+            sg.Radio("threshold", "Radio", size=(10, 1), key="-THRESH-"),
+            sg.Slider(
+                (0, 255),
+                128,
+                1,
+                orientation="h",
+                size=(40, 15),
+                key="-THRESH SLIDER-",
+            ),
+        ],
+        [
+            sg.Radio("canny", "Radio", size=(10, 1), key="-CANNY-"),
+            sg.Slider(
+                (0, 255),
+                128,
+                1,
+                orientation="h",
+                size=(20, 15),
+                key="-CANNY SLIDER A-",
+            ),
+            sg.Slider(
+                (0, 255),
+                128,
+                1,
+                orientation="h",
+                size=(20, 15),
+                key="-CANNY SLIDER B-",
+            ),
+        ],
+        [
+            sg.Radio("blur", "Radio", size=(10, 1), key="-BLUR-"),
+            sg.Slider(
+                (1, 11),
+                1,
+                1,
+                orientation="h",
+                size=(40, 15),
+                key="-BLUR SLIDER-",
+            ),
+        ],
+        [
+            sg.Radio("hue", "Radio", size=(10, 1), key="-HUE-"),
+            sg.Slider(
+                (0, 225),
+                0,
+                1,
+                orientation="h",
+                size=(40, 15),
+                key="-HUE SLIDER-",
+            ),
+        ],
+        [
+            sg.Radio("enhance", "Radio", size=(10, 1), key="-ENHANCE-"),
+            sg.Slider(
+                (1, 255),
+                128,
+                1,
+                orientation="h",
+                size=(40, 15),
+                key="-ENHANCE SLIDER-",
+            ),
+        ],
+    ]
+
+    # Create the window and show it without the plot
+    window = sg.Window("Configuration panel", layout, location=(700, 350))
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        event, values = window.read(timeout=20)
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+
+        ret, frame = cap.read()
+
+    window.close()
 
 
 def on_clicked(icon, item):
-    if str(item) == "Say Hello":
-        print("Hello World")
+    if str(item) == "Start":
+        print("App start running...")
+    elif str(item) == "Settings":
+        conf()
     elif str(item) == "Exit":
         icon.stop()
 
+
+image = Image.open(iconDir)
+
 icon = pystray.Icon("EmoSupporter", image, menu=pystray.Menu(
-    pystray.MenuItem("Say Hello", on_clicked),
-    pystray.MenuItem("Exit", on_clicked
+    pystray.MenuItem("Start", on_clicked),
+    pystray.MenuItem("Settings", on_clicked),
+    pystray.MenuItem("Exit", on_clicked)
 ))
 
 icon.run()
